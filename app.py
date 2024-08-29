51318463d2a5c62971abcb0e20c555bfc2cc2bd2
AlexanderOllman <alexander.ollman@gmail.com>
2024-08-29 15:45:31 -0700

    auth through request header

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from langchain.schema import AIMessage, HumanMessage
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 import gradio as gr
 import os
+import requests
 
-url = "https://llama3-8b-instruct-1xgpu-predictor-admin-835cc1e3.prod.discover.hpepcai.com/v1"
-model = "meta/llama3-8b-instruct"
-nvidia_api_key = os.getenv("AUTH_TOKEN")
-llm = ChatNVIDIA(base_url=url, model=model, api_key=nvidia_api_key)
-
-def predict(message, history):
+def predict(message, history, request: gr.Request):
     history_langchain_format = []
     for human, ai in history:
         history_langchain_format.append(HumanMessage(content=human))
         history_langchain_format.append(AIMessage(content=ai))
     history_langchain_format.append(HumanMessage(content=message))
+    url = "https://llama3-8b-instruct-1xgpu-predictor-admin-835cc1e3.prod.discover.hpepcai.com/v1"
+    model = "meta/llama3-8b-instruct"
+    nvidia_api_key = request.headers.get("authorization")
+    llm = ChatNVIDIA(base_url=url, model=model, api_key=nvidia_api_key)
     gpt_response = llm.invoke(history_langchain_format)
     return gpt_response.content
 
